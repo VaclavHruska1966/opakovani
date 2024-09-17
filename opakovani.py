74ad28d407ac159f3940f4bb060d37920f31b4d8
Václav Hruška <Vaclav.Hruska@student.souepl.cz>
2024-09-17 13:43:59 +0200

    opakovani

--- opakovani.py
+++ opakovani.py
@@ -1,24 +1,2 @@
-import random
-
-pole1 = []
-pole2 = []
-
-for i in range(random.randint(1,10)):
-    pole1.append(random.randint(1, 10))
-    pole1.sort()
-print(pole1)
-
-for j in range(random.randint(1,10)):
-    pole2.append(random.randint(1, 10))
-    pole2.sort()
-print(pole2)
-
-
-
-if pole1<pole2:
-    print ("pole1 je větší než pole2")
-
-
-elif pole1 > pole2:
-    print("pole2 je větší než pole1")
-
+import math
+print(eval(input("Enter calculation: "), {"math": math}))
